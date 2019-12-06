c43eae5a69da367e1cef78842636bc887621edeb
Joshua <organizedchaos20@gmail.com>
2019-12-06 12:18:06 -0500

    fixed one pass with no data (had '-' and was turning it into a character column); made some updates to the nextGen script (mostly can more easily make defensive charts now)

--- nextGenViz.R
+++ nextGenViz.R
@@ -8,98 +8,57 @@ save <- '../next-gen-scrapy/compositeCharts'
 
 all <- read_csv('../next-gen-scrapy/pass_locations.csv') %>%
   mutate(pass_type = factor(pass_type, levels = c('COMPLETE', 'INCOMPLETE', 'INTERCEPTION', 'TOUCHDOWN')))
-
 lamarQT <- read_csv('../next-gen-scrapy/lamarWithQT.csv') %>%
   mutate(pass_type = factor(pass_type, levels = c('COMPLETE', 'INCOMPLETE', 'INTERCEPTION', 'TOUCHDOWN')))
 
-anti_join(df %>%
-            mutate(date = as.Date(date, '%Y.%m.%d')) %>%
-            filter(type == 'pass',
-                   passer == 'L.Jackson'),
-          lamarQT,
-          by=c('date', 'q', 'time', 'detail'),
+teamAbb <- read_csv('teamAbb.csv')
+ravensD <- inner_join(all, inner_join(read_csv('ravens.csv'), teamAbb),
+                      by = c('week', 'team'))
+oppD <- inner_join(all, inner_join(read_csv('opp.csv'), teamAbb),
+                   by = c('week', 'team'))
+
+anti_join(seasonDF %>% mutate(date = as.Date(date, '%Y.%m.%d')) %>%
+            filter(type == 'pass', passer == 'L.Jackson'),
+          lamarQT, by=c('date', 'q', 'time', 'detail'),
           suffix=c('', '_data')) %>%
-  filter(possession == 'BLT',
-         !is.na(type)) %>%
+  filter(possession == 'BLT', !is.na(type)) %>%
   select(date, q, time, detail) %>%
   arrange(date)
 
-lamarWithAllData <- inner_join(df %>%
-                                 mutate(date = as.Date(date, '%Y.%m.%d')),
+lamarWithAllData <- inner_join(seasonDF %>% mutate(date = as.Date(date, '%Y.%m.%d')),
                                lamarQT,
                                by=c('date', 'q', 'time', 'detail'),
                                suffix=c('', '_data'))
 
-
-for (each in lamarWithAllData %>%
-     distinct(target) %>%
-     pull(target)) {
-  if ((each != 'spike') & !is.na(each)) {
-    print (each)
-    make_Composite_Charts_For_Receiver(lamarWithAllData, each, save)
+for (receiver in lamarWithAllData %>% distinct(target) %>% pull(target)) {
+  if ((receiver != 'spike') & !is.na(receiver)) {
+    make_Composite_Charts_For_Receiver(lamarWithAllData, receiver, save)
   }
 }
 
 for (each in seq(1,4)) {
   make_Composite_Charts_For_QB(lamarWithAllData %>%
-                        filter(down == each), 'all', save, sprintf('Down %d', each))
+                        filter(down == each), 'all', save, sprintf('Lamar/Down %d', each))
 }
 
 lamarTD <- lamar %>%
   filter(pass_type == 'TOUCHDOWN')
-
 make_Composite_Charts_For_QB(lamarTD, 'Lamar Jackson', save, 'Lamar Jackson TDs')
 
-for (each in all %>%
-     distinct(name) %>%
-     pull(name)) {
+for (each in all %>% distinct(name) %>% pull(name)) {
   make_Composite_Charts_For_QB(all, each, save, each)
 }
 
-for (each in all %>%
-     distinct(name) %>%
-     pull(name)) {
+for (each in all %>% distinct(name) %>% pull(name)) {
   make_Composite_Charts_For_QB(all %>% 
                                  filter(pass_type == 'TOUCHDOWN'), each, save, sprintf('TDs %s', each))
 }
-print (sprintf('TDs %s', each))
-
-ravensD <- all %>%
-  filter((team == 'miami-dolphins' & week == 1)|
-           (team == 'arizona-cardinals' & week == 2)|
-           (team == 'kansas-city-chiefs' & week == 3)|
-           (team == 'cleveland-browns' & week == 4)|
-           team == 'pittsburgh-steelers' & week == 5|
-           team == 'cincinnati-bengals' & week == 6|
-           team == 'seattle-seahawks' & week == 7|
-           team == 'new-england-patriots' & week == 9|
-           team == 'cincinnati-bengals' & week == 10)
 
 make_Chart(ravensD, "Ravens D", '0', write_Tweet_Content(ravensD))
 ggsave(file=sprintf('%s/%s plot.png', save, 'Ravens D'), width=11.5, height=8)
 
-opp <- list('1' = 'new-orleans-saints',
-            '2' = 'jacksonville-jaguars',
-            '3' = 'los-angeles-chargers',
-            '4' = 'carolina-panthers',
-            '5' = 'atlanta-falcons',
-            '6' = 'kansas-city-chiefs',
-            '7' = 'indianapolis-colts',
-            '8' = 'oakland-raiders',
-            '9' = 'jacksonville-jaguars')
 
-oppD <- all %>%
-  filter(week == names(opp)[1] & team == opp[[1]]|
-           week == names(opp)[2] & team == opp[[2]]|
-           week == names(opp)[3] & team == opp[[3]]|
-           week == names(opp)[4] & team == opp[[4]]|
-           week == names(opp)[5] & team == opp[[5]]|
-           week == names(opp)[6] & team == opp[[6]]|
-           week == names(opp)[7] & team == opp[[7]]|
-           week == names(opp)[8] & team == opp[[8]]|
-           week == names(opp)[9] & team == opp[[9]])
-
-oppName = 'TexansD'
+oppName = 'BillD'
 make_Chart(oppD, oppName, '0', write_Tweet_Content(oppD))
 ggsave(file=sprintf('%s/%s plot.png', save, oppName), width=11.5, height=8)
 
@@ -119,31 +78,8 @@ oppD %>%
             td = sum(pass_type %in% c('TOUCHDOWN')),
             int = sum(pass_type %in% c('INTERCEPTION')))
 
-oppD %>%
-  filter(y >= 20,
-         pass_type %in% c('INTERCEPTION'))
-
-oppD %>%
-  filter(y >= 20,
-         pass_type %in% c('COMPLETE', 'TOUCHDOWN'))
-oppD %>%
-  filter(y >= 20) %>%
-  summarize(Att = n(),
-            Com = sum(pass_type %in% c('COMPLETE', 'TOUCHDOWN')),
-            td = sum(pass_type %in% c('TOUCHDOWN')),
-            int = sum(pass_type %in% c('INTERCEPTION')))
-all %>%
-  filter(y >= 20) %>%
-  summarize(Att = n(),
-            Com = sum(pass_type %in% c('COMPLETE', 'TOUCHDOWN')),
-            td = sum(pass_type %in% c('TOUCHDOWN')),
-            int = sum(pass_type %in% c('INTERCEPTION')))
-
-make_Composite_Charts_For_QB(oppD, 'all', save, oppName)
 make_Composite_Charts_For_QB(all, 'all', save, 'all')
 make_Chart(all, "all", '0')
-make_Chart(all %>%
-             filter(week == 6), "week 6", '0')
 
 fitData <- all %>%
   mutate(C = case_when(pass_type %in% c('COMPLETE', 'TOUCHDOWN')~1,
@@ -175,7 +111,8 @@ all %>%
             TD = sum(TD),
             INT = sum(INT)) %>%
   arrange(zone, -Att) %>% View()
-
+make_Zone_Chart(ravensD)
+make_Zone_Chart(oppD)
 
 NFL <- gam(C~s(abs(x),df=6)+y, data=fitData)
 summary(NFL)
@@ -218,32 +155,6 @@ write.csv(inner_join(lamarZone, league, by="vertZone",
             select(1,5,10,6,11,2,7,3,8,4,9),
           file = 'lamar vs league.csv',
           row.names=F)
-
-write.csv(lamarWithAllData %>%
-  filter(target =='M.Boykin') %>%
-  select(c(date, q, time, y, pass_type)) %>%
-  arrange(-y),
-  file = 'for Ken/temp.csv',
-  row.names=F)
-
-df %>%
-  filter(passer == 'L.Jackson',
-         type == 'pass',
-         down == 1,
-         date == '2019.09.08')# %>%
-#  group_by(date) %>%
-  summarize(Com = sum(complete, na.rm=T),
-            Att = n(),
-            gain = sum(gain, na.rm=T))
-
-df %>%
-  filter(passer == 'L.Jackson',
-         down == 1,
-         gain >= togo)
-df %>%
-  filter(gain == 21,
-         type == 'pass')
-
 
 all %>%
   filter(y<0) %>%
@@ -303,41 +214,3 @@ ggplot(intBehindLoS, aes(x, y)) +
   geom_image(aes(image=cols), size=.075)
 
 ggsave(file=sprintf('%s/%s plot.png', save, 'Int Behind LoS'), width=11.5, height=8)
-
-all %>%
-  filter(name == 'Lamar Jackson' ) %>%
-  mutate(Depth = case_when(y >= 15~ 'deep',
-                           y < 15 ~ 'short',
-                           TRUE ~ 'NA')) %>%
-  group_by(Depth) %>%
-  summarize(Com = sum(pass_type %in% c('COMPLETE', 'TOUCHDOWN')),
-            Att = n(),
-            TD = sum(pass_type == 'TOUCHDOWN'),
-            INT = sum(pass_type == 'INTERCEPTION'))
-
-lamar %>%
-  mutate(Direction = cut(x, 5)) %>%
-  group_by(Direction) %>%
-  tally
-
-all %>%
-  mutate(Direction = cut(x, 3)) %>%
-  filter(!is.na(Direction)) %>%
-  group_by(name, Direction) %>%
-  summarize(Att = n()) %>%
-  mutate(PercentOfThrows = Att/sum(Att)) %>%
-  arrange(-PercentOfThrows) %>% View()
-
-combined %>% filter(Skill5 == 89, down==3) %>%
-  group_by(type) %>%
-  tally %>%
-  filter(type %in% c('pass', 'sack', 'scramble', 'run')) %>%
-  mutate(Per = n/sum(n)) %>%
-  arrange(-n)
-
-
-lamarWithAllData 
-
-
-make_Composite_Charts_For_QB(all %>%
-                               filter(week > 3), 'Lamar Jackson', save, 'Lamar Since Week 4')
